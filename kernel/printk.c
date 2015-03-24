faa1e8b184eadef60ab0e7e1d1aa76d7ed378880
Hybridmax <hybridmax95@gmail.com>
2015-03-24 16:31:33 +0100

    Linux 3.4.81 - 3.4.90

--- kernel/printk.c
+++ kernel/printk.c
@@ -1142,9 +1142,9 @@ static int console_trylock_for_printk(unsigned int cpu)
 		}
 	}
 	printk_cpu = UINT_MAX;
+	raw_spin_unlock(&logbuf_lock);
 	if (wake)
 		up(&console_sem);
-	raw_spin_unlock(&logbuf_lock);
 	return retval;
 }
 static const char recursion_bug_msg [] =
@@ -1522,6 +1522,7 @@ static int __cpuinit console_cpu_notify(struct notifier_block *self,
 {
 	switch (action) {
 	case CPU_DEAD:
+	case CPU_DYING:
 	case CPU_DOWN_FAILED:
 	case CPU_UP_CANCELED:
 		console_lock();
